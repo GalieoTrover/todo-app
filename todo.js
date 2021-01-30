9616cb3cfd27982f3a0169f16a83e1564ba35357
Galieo Trover <58079706+GalieoTrover@users.noreply.github.com>
2021-01-30 09:18:41 +0000

    Add files via upload

--- todo.js
+++ todo.js
@@ -12,20 +12,34 @@ btn.addEventListener("click", function () {
   // let div = document.createElement("div");
   // let para = document.createElement("p");
 
-  li.innerText = box.value;
-  link.innerHTML = '<i class="fa fa-remove"></i>';
-
-  cont.appendChild(ul);
-  ul.appendChild(li);
-  // li.appendChild(para);
-  li.appendChild(link);
   // cont.insertAdjacentElement("beforeend", div);
   // cont.appendChild(div);
 
-  let cross = document.querySelector("i.fa-remove");
-  cross.addEventListener("click", function () {
-    li.innerText = "";
+  // let cross = document.querySelector("i.fa-remove");
+  // cross.addEventListener("click", function () {
+  //   li.innerText = "";
+  // });
+
+  if (box.value !== "") {
+    li.innerText = box.value;
+    li.classList.add("para-styling");
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+
+    cont.appendChild(ul);
+    ul.appendChild(li);
+    // li.appendChild(para);
+    li.appendChild(link);
+  }
+
+  let remove = document.querySelectorAll(".fa-remove");
+  remove.forEach(function (e) {
+    e.addEventListener("click", function () {
+      ul.removeChild(li);
+    });
   });
+  // li.addEventListener("click", function () {
+  //   li.style.textDecoration = "line-through";
+  // });
 
   box.value = "";
   box.focus();
