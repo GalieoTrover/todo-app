caf1a4befd20122549cb6c7d78912a6f0cdbff44
Galieo Trover <58079706+GalieoTrover@users.noreply.github.com>
2021-02-17 21:38:54 +0530

    cleaned up

--- todo.js
+++ todo.js
@@ -9,41 +9,19 @@ btn.addEventListener("click", function () {
   let li = document.createElement("li");
   let link = document.createElement("a");
 
-  // let div = document.createElement("div");
-  // let para = document.createElement("p");
-
-  // cont.insertAdjacentElement("beforeend", div);
-  // cont.appendChild(div);
-
-  // let cross = document.querySelector("i.fa-remove");
-  // cross.addEventListener("click", function () {
-  //   li.innerText = "";
-  // });
-
   if (box.value !== "") {
     li.innerText = box.value;
-    li.classList.add("para-styling");
+    li.classList.add("list-styling");
     link.innerHTML = '<i class="fa fa-remove"></i>';
 
     cont.appendChild(ul);
     ul.appendChild(li);
-    // li.appendChild(para);
-    li.appendChild(link);
+    list.appendChild(link);
   }
-
-  // let remove = document.querySelectorAll(".fa-remove");
-  // remove.forEach(function (e) {
-  //   e.addEventListener("click", function () {
-  //     ul.removeChild(li);
-  //   });
-  // });
 
   link.addEventListener("click", function () {
     ul.removeChild(li);
   });
-  // li.addEventListener("click", function () {
-  //   li.style.textDecoration = "line-through";
-  // });
 
   box.value = "";
   box.focus();
